db3bc5ea2f00ede5a1829ecd2634b7b82db89c8f
Ananth Madhavan <ananthmadhavan6@gmail.com>
2014-07-07 15:23:10 +0530

    Implement remember me.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		puts 
+		
 		user = User.find_by_email(params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
 			sign_in user
